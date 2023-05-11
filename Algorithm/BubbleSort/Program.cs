b53722d0c19256f9b90576d31f896e3155b67a33
LimTaeHun <lsw463@naver.com>
2023-05-12 01:51:19 +0900

    Feat: BubbleSort 구현

--- Algorithm/BubbleSort/Program.cs
+++ Algorithm/BubbleSort/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿int[] array = {20,15,1,5, 10};
 
-Console.WriteLine("Hello, World!");
+ShowArray();
+
+// array의 길이 -1 만큼 반복
+for (int i = 0; i < array.Length - 1; i++)
+{
+    // array길이-i-1 만큼 반복
+    for (int j = 0; j < array.Length - i - 1; j++)
+    {
+        // j번째 값이 j+1번째 값보다 클 떄 서로 스왑 한다
+        if (array[j] > array[j + 1])
+        {
+            (array[j], array[j + 1]) = (array[j + 1], array[j]);
+        }
+    }
+}
+
+ShowArray();
+
+void ShowArray()
+{
+    Console.Write("Array : ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    
+    Console.WriteLine();
+}
